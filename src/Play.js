04807c5f3e5440bdf39eddaaff6b408ad9755d23
dexhoang <dhhoang@ucsc.edu>
2025-04-05 22:56:20 -0700

    Added player spritesheet & camera

--- src/Play.js
+++ src/Play.js
@@ -11,12 +11,26 @@ class Play extends Phaser.Scene {
         this.load.path = './assets/'
         this.load.image('tileSetImage', 'lab_tileset.png')
         this.load.tilemapTiledJSON('tilemapJSON', 'MarsMapin.json') 
+
+        this.load.spritesheet('player', 'playerWalk.png', {
+            frameWidth: 64,
+            frameHeight: 64
+        })
     }
 
     create() {
         const map = this.add.tilemap('tilemapJSON')
         const tileset = map.addTilesetImage('tiles1', 'tileSetImage')
         const bgLayer = map.createLayer('Tile Layer 1', tileset, 0, 0)
+
+        //player
+        const playerSpawn = map.findObject('Spawns', (obj) => obj.name === 'spawn')
+        this.player = this.physics.add.sprite(playerSpawn.x, playerSpawn.y, 'player', 0)
+
+        //camera
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+        this.cameras.main.startFollow(this.player, true, 0.25, 0.25)
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
     }
 
     update() {
